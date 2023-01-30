f20947675b12bada865ae2d8d7c76419341b9705
tsrkzy <tsrmix@gmail.com>
2023-01-30 12:44:19 +0900

    ecs wakaran

--- go_echo/go_reload.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# docker(go_echo)の中にairが入っているので、基本的にリロードは意識しなくて良い
-# @REF https://github.com/cosmtrek/air
-
-echo 'go build... '
-docker exec -it go_echo go mod tidy
-docker exec -it go_echo go build -v -o /usr/local/bin/app
-echo 'done!'
